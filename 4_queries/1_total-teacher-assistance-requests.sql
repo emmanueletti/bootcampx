0a06a073b726dd3bc9132102bcbd15ad55c0f3ee
Emmanuel Etti <emmanueletti@gmail.com>
2021-07-20 08:34:47 -0700

    adds quesries 4 folder

---
+++ 4_queries/1_total-teacher-assistance-requests.sql
@@ -0,0 +1,12 @@
+-- Get the total number of assistance_requests for a teacher.
+-- Select the teacher's name and the total assistance requests.
+-- Since this query needs to work with any specific teacher name, use 'Waylon Boehm' for the teacher's name here.
+
+SELECT 
+  a_r.id, 
+  a_r.teacher_id, 
+  t.name
+FROM assistance_requests a_r 
+JOIN teachers t
+  ON a_r.teacher_id = t.id
+WHERE t.name = 'Waylon Boehm'
